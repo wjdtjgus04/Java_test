e4761cda9c524a5e66635cec7d0fb9611425661a
aline <aline2385@naver.com>
2023-10-05 21:58:38 +0900

    commit

--- pratice/src/pratice/problems.java
+++ pratice/src/pratice/problems.java
@@ -4,6 +4,7 @@ public class problems {
 
 	public static void main(String[] args) {
 		System.out.print("hello, github");
+		System.out.print("practice");
 		// TODO Auto-generated method stub
 
 	}

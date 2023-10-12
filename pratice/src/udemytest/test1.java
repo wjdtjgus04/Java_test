2df6f79a0008f442dcf3d846c02ce88682f568dc
aline <seohyunj983@gmail.com>
2023-10-12 13:15:09 +0900

    multiplication table

---
+++ pratice/src/udemytest/test1.java
@@ -0,0 +1,17 @@
+package udemytest;
+import java.util.*;
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		
+		for (int i =1; i<11; i++) {
+			System.out.println(num1+" * "+i+" = "+(num1*i));              
+			
+		}
+sc.close();
+	}
+
+}

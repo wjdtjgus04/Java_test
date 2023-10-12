7fe40fd2a3c87d73957e1e363209ae4e37ccac48
aline <seohyunj983@gmail.com>
2023-10-12 19:06:18 +0900

    angle3

---
+++ pratice/src/udemytest/exercise20231012_7.java
@@ -0,0 +1,21 @@
+package udemytest;
+import java.util.*;
+public class exercise20231012_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int angle1 = sc.nextInt();
+		int angle2 = sc.nextInt();
+		int angle3 = sc.nextInt();
+		
+		if (angle1+angle2+angle3 == 180) {
+			
+		System.out.printf("완벽한 삼각형입니다");}
+		
+		sc.close();
+
+	}
+
+}

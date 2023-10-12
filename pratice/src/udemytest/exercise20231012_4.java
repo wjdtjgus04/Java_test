8312cfa3afe12b30f11d14d4e376dff84986d45b
aline <seohyunj983@gmail.com>
2023-10-12 17:05:03 +0900

    printf

---
+++ pratice/src/udemytest/exercise20231012_4.java
@@ -0,0 +1,14 @@
+package udemytest;
+
+public class exercise20231012_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int hour = 24;
+		int minute = 60;
+		System.out.println("하루는 몇분입니까?>> "+(hour*minute)+"분" );
+
+	}
+
+}

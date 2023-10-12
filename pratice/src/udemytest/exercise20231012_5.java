c16e71b47b7823af1d24fd0dc9c242b3f59630a7
aline <seohyunj983@gmail.com>
2023-10-12 19:09:05 +0900

    a+b+c

--- pratice/src/udemytest/exercise20231012_5.java
+++ pratice/src/udemytest/exercise20231012_5.java
@@ -5,13 +5,11 @@ public class exercise20231012_5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
-		System.out.println("5*3 = "+5*3+"\n");
-		
-		for(int i = 1; i<11; i++) {
-			System.out.println("5 * "+i+" = "+5*i);
-		}
-		
+		int a = 10;
+		int b=20;
+		int c = 30;
+		System.out.printf("%d + %d+%d = %d ", a,b,c,a+b+c);
+
 	}
 
 }

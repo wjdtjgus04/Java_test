79d54f3f0edbd216776f0ea2b95f8d1b6096c541
aline <seohyunj983@gmail.com>
2023-10-12 14:24:09 +0900

    print1

---
+++ pratice/src/udemytest/exercise20231012_3.java
@@ -0,0 +1,11 @@
+package udemytest;
+
+public class exercise20231012_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+	}
+
+}

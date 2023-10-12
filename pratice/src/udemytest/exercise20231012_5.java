a79402eabee1c7c7e44583898154517ef362638e
aline <seohyunj983@gmail.com>
2023-10-12 18:59:46 +0900

    a+b

---
+++ pratice/src/udemytest/exercise20231012_5.java
@@ -0,0 +1,17 @@
+package udemytest;
+
+public class exercise20231012_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+		System.out.println("5*3 = "+5*3+"\n");
+		
+		for(int i = 1; i<11; i++) {
+			System.out.println("5 * "+i+" = "+5*i);
+		}
+		
+	}
+
+}

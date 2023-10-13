b6eea29e83aaffabefa9e46ac032e13e749f3bf5
aline <seohyunj983@gmail.com>
2023-10-13 22:08:29 +0900

    7*i

---
+++ pratice/src/udemytest/exercise20231012_9.java
@@ -0,0 +1,13 @@
+package udemytest;
+public class exercise20231012_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<11; i++) {
+			System.out.println("7 * "+i+" = "+7*i);
+		}
+
+	}
+
+}

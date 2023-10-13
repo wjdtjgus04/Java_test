dcde706906173462c55c4bd00182c43ab4d0b6f6
aline <seohyunj983@gmail.com>
2023-10-13 21:19:35 +0900

    %2

---
+++ pratice/src/udemytest/exercise20231012_8.java
@@ -0,0 +1,22 @@
+package udemytest;
+import java.util.*;
+public class exercise20231012_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		
+		if (num%2==0) {
+			
+		System.out.printf("Â¦¼ö");}
+		else
+			System.out.printf("È¦¼ö");
+		
+		sc.close();
+
+	}
+
+}

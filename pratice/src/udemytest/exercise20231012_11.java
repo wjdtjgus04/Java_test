57cd6f64cfc0afd8133d6b426f85276a305e6af1
aline <seohyunj983@gmail.com>
2023-10-13 22:19:42 +0900

    10 to 1

---
+++ pratice/src/udemytest/exercise20231012_11.java
@@ -0,0 +1,17 @@
+package udemytest;
+
+public class exercise20231012_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int j=10; j>0; j--) {
+			
+				System.out.println(j);
+			}	
+			
+			
+
+	}
+
+}
